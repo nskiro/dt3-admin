b9600cf7c14771515c2d49f998af158eb2d8e816
Minh Tam <minhtam@SVR3.local>
2018-06-15 14:17:33 +0700

    cập nhật menu-role

--- src/pages/Managements/RolePage/RoleForm.js
+++ src/pages/Managements/RolePage/RoleForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Row, Col, Form, Input, Button, Table, Divider, Modal, Popconfirm } from 'antd'
+import { Row, Col, Form, Input, Button, Table, Divider, Modal, Popconfirm, TreeSelect } from 'antd'
 import _ from 'lodash'
 import axios from '../../../axiosIns'
 
@@ -26,21 +26,40 @@ const columns = [
     render: (text, record) => (
       <span>{text === null ? '' : new Date(text).toLocaleDateString('vi-VN')}</span>
     ),
-  },
-]
+  }, {
+    title: 'Menu',
+    dataIndex: 'menu',
+    render: (text, record) => (
+      <span>
+        {text.map((obj) => {
+          return <p key={obj._id}>{obj.menu_label}</p>
+        })}
+      </span>
+    ),
+  }
+];
+
+const converToTreeNode = (data) => {
+  return data.map((obj) => {
+    if (obj.children) {
+      return { title: obj.menu_label, key: obj._id, value: obj._id, children: converToTreeNode(obj.children) }
+    }
+    return { title: obj.menu_label, key: obj._id, value: obj._id };
+  })
+}
 
 class AddRoleForm extends Component {
   render() {
     const { getFieldDecorator } = this.props.form
 
     return (
-      <Form onSubmit={this.props.handleSubmit} layout="horizontal">
-        <FormItem label="Role Name" {...formItemLayout}>
+      <Form onSubmit={this.props.handleSubmit} layout="inline">
+        <FormItem label="Role Name">
           {getFieldDecorator('roleName', {
             rules: [{ required: true, message: 'Please input role name!' }],
           })(<Input />)}
         </FormItem>
-        <FormItem {...formItemLayout}>
+        <FormItem>
           <Button type="primary" htmlType="submit">
             Add Role
           </Button>
@@ -53,13 +72,20 @@ class AddRoleForm extends Component {
 class UpdateRoleForm extends Component {
   render() {
     const { getFieldDecorator } = this.props.form
-    const { data } = this.props
+    const { data, treeData } = this.props
+    let menu = [];
+    if(data.length > 0 && data[0].menu.length > 0){
+      menu = data[0].menu.map((obj)=>{
+        return obj._id;
+      })
+    }
     return (
       <Modal
         title="Update Role"
         visible={this.props.visible}
         onOk={this.props.onOk}
         onCancel={this.props.onCancel}
+        destroyOnClose
       >
         <Form onSubmit={this.props.handleSubmit} layout="horizontal">
           <FormItem>
@@ -68,13 +94,26 @@ class UpdateRoleForm extends Component {
             )}
           </FormItem>
           <FormItem label="Role Name" {...formItemLayout}>
-            {getFieldDecorator(
-              'roleName',
-              { initialValue: data.length > 0 ? data[0].role_name : '' },
-              {
-                rules: [{ required: true, message: 'Please input role name!' }],
-              },
-            )(<Input />)}
+            {
+              getFieldDecorator('role_name',
+                { initialValue: data.length > 0 ? data[0].role_name : '' },
+                {
+                  rules: [{ required: true, message: 'Please input role name!' }],
+                },
+              )(<Input />)}
+          </FormItem>
+          <FormItem label="Menu" {...formItemLayout}>
+            {
+              getFieldDecorator('menu',
+                { initialValue: menu },
+              )(
+                <TreeSelect
+                  style={{ width: 300 }}
+                  placeholder="Please select"
+                  treeData={treeData}
+                  treeCheckable
+                />
+              )}
           </FormItem>
         </Form>
       </Modal>
@@ -88,6 +127,8 @@ const WrappedUpdateRoleForm = Form.create()(UpdateRoleForm)
 class RoleForm extends Component {
   state = {
     roleList: [],
+    menu_data: [],
+    selectedMenu: [],
     selectedRows: [],
     selectedRowKeys: [],
     showUpdateModal: false,
@@ -107,8 +148,33 @@ class RoleForm extends Component {
       })
       .catch(err => {
         console.log(err)
+      });
+    axios.get('api/admin/menu/getroot')
+      .then(res => {
+        let rs = res.data;
+        if (rs.valid) {
+          let menu_data = [];
+          menu_data = converToTreeNode(rs.data);
+          this.setState({ menu_data: menu_data });
+        } else {
+          this.setState({ menu_data: [] });
+          alert(rs.message);
+        }
+      })
+      .catch(err => {
+        console.log(err);
       })
   }
+
+
+  showUpdateModal = () => {
+    this.setState({ showUpdateModal: true })
+  }
+
+  closeUpdateModal = () => {
+    this.setState({ showUpdateModal: false })
+  }
+
   handleSubmit = e => {
     e.preventDefault()
     this.addRoleForm.validateFields((err, values) => {
@@ -126,14 +192,6 @@ class RoleForm extends Component {
           })
       }
     })
-  }
-
-  showUpdateModal = () => {
-    this.setState({ showUpdateModal: true })
-  }
-
-  closeUpdateModal = () => {
-    this.setState({ showUpdateModal: false })
   }
 
   handleUpdate = () => {
@@ -171,59 +229,68 @@ class RoleForm extends Component {
       })
   }
 
+  onChangeTreeNode = (value) => {
+    this.setState({ selectedMenu: value })
+  }
+
   render() {
-    const { roleList, selectedRows, showUpdateModal } = this.state
+    const { roleList, menu_data, selectedRows, showUpdateModal } = this.state
     return (
-      <Row>
-        <Col span={10}>
-          <WrappedAddRoleForm
-            ref={node => (this.addRoleForm = node)}
-            handleSubmit={this.handleSubmit}
-          />
-          <WrappedUpdateRoleForm
-            ref={node => (this.updateRoleForm = node)}
-            visible={showUpdateModal}
-            onOk={this.handleUpdate}
-            onCancel={this.closeUpdateModal}
-            data={selectedRows}
-          />
-        </Col>
-        <Col span={14}>
-          <Button
-            icon="edit"
-            size="small"
-            onClick={this.showUpdateModal}
-            disabled={selectedRows.length === 0 || selectedRows.length > 1 ? true : false}
-          >
-            Edit
-          </Button>
-          <Divider type="vertical" />
-          <Popconfirm
-            title="Are you sure delete?"
-            onConfirm={this.handleDelete}
-            okText="Yes"
-            cancelText="No"
-          >
+      <span>
+        <Row>
+          <Col span={24}>
+            <WrappedAddRoleForm
+              ref={node => (this.addRoleForm = node)}
+              handleSubmit={this.handleSubmit}
+            />
+            <WrappedUpdateRoleForm
+              ref={node => (this.updateRoleForm = node)}
+              visible={showUpdateModal}
+              onOk={this.handleUpdate}
+              onCancel={this.closeUpdateModal}
+              data={selectedRows}
+              treeData={menu_data}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
             <Button
-              icon="close"
-              type="danger"
+              icon="edit"
               size="small"
-              disabled={selectedRows.length === 0 ? true : false}
+              onClick={this.showUpdateModal}
+              disabled={selectedRows.length === 0 || selectedRows.length > 1 ? true : false}
             >
-              Delete
+              Edit
+          </Button>
+            <Divider type="vertical" />
+            <Popconfirm
+              title="Are you sure delete?"
+              onConfirm={this.handleDelete}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button
+                icon="close"
+                type="danger"
+                size="small"
+                disabled={selectedRows.length === 0 ? true : false}
+              >
+                Delete
             </Button>
-          </Popconfirm>
-          <Table
-            style={{ marginTop: '5px' }}
-            rowKey={'_id'}
-            columns={columns}
-            dataSource={roleList}
-            rowSelection={this.rowSelection}
-            size="small"
-            bordered
-          />
-        </Col>
-      </Row>
+            </Popconfirm>
+            <Table
+              style={{ marginTop: '5px' }}
+              rowKey={'_id'}
+              columns={columns}
+              dataSource={roleList}
+              rowSelection={this.rowSelection}
+              size="small"
+              bordered
+            />
+          </Col>
+        </Row>
+      </span>
     )
   }
 }
